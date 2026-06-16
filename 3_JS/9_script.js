137a4e4fd44617418357b8cde15c614865063624
himanshu <vinitabg2@gmail.com>
2026-06-16 15:09:06 +0530

    16th June

--- 3_JS/9_script.js
+++ 3_JS/9_script.js
@@ -4,9 +4,9 @@ console.log(c); // returns a string without changing value of a
 
 
 console.log(a.toPrecision(4)); // returns 4 numbers form starting of a
- 
+      
 console.log(a.toString());
-
+   
  
 let d = new Number(20); // creates a number as an obje ct
 console.log(typeof(d));
