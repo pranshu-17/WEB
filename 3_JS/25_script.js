96fbee2a801df5b3b840e9c307e72481e3f2aba6
himanshu <vinitabg2@gmail.com>
2026-06-29 17:57:52 +0530

    29th June

--- 3_JS/25_script.js
+++ 3_JS/25_script.js
@@ -1,4 +1,4 @@
-/* HOISTING IN JAVASCRIPT 
+/* HOISTING IN JAVASCRIPT : Hoisting is a JavaScript behavior where variable and function declarations are available before their actual declaration in the code. For example, variables declared with var are hoisted and initialized with undefined, while let and const are hoisted but cannot be accessed before their declaration. Function declarations are fully hoisted, so they can be called before they are defined in the code."
 
 1) whenever we run a code in js => execution context is created
 
@@ -6,7 +6,7 @@
 
 3) Memory allocation phase: allocates memory to variables and stores "undefined" in that memory space and for functions it allocates the memory for it in heap and stores the whole function code in that memory(stores the address of this memory in stack)
 
-- first of all the global variables will be allocated the memory, and functional variables will get the memory when the function gets called
+- first of all the global variables will be allocated the memory, and functional variables will get the memory when the function gets called   
 
 4) Execution Phase : reads the code again and now variables will get their values and functions will be called
 
@@ -34,12 +34,25 @@
 
 // example (same for const and let);
 
+/* MEMORY ALLOCATION PHASE : 
+a = <uninitialised> (Temporal Dead Zone)
+b = <uninitialised> (Temporal Dead Zone)
+add = <uninitialised> (Temporal Dead Zone)
+
+EXECUTION PHASE : 
+a = 10
+b = 20
+then throws error because add is still in TDZ
+
+*/
+
 let a = 10;
 let b = 20;
+
+console.log(add(a,b));
 
 const add = function(num1, num2){
     const sum = num1 + num2;
     return sum;
 }
 
-console.log(add(a,b));
